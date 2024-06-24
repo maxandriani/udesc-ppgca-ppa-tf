3fcd9fee9e7e197ec3899da9f42255418c95d3c2
Maxmiliano <max.andriani@gmail.com>
2024-06-24 01:56:41 -0300

    feat: Correções no runtime de benchmark

--- src/App.Benchmark/BenchmarkContext.cs
+++ src/App.Benchmark/BenchmarkContext.cs
@@ -1,21 +1,25 @@
 using BenchmarkDotNet.Attributes;
-using Commands;
 using Core.IO;
 using Core.Nfs;
 using Core.Operacoes;
 using Core.Produtos;
 using Core.Regimes;
 
-namespace Benchmarks;
-
 public class BenchmarkContext
 {
-    public static DocumentReader<Bom>? bomReader;
-    public static DocumentReader<Nf>? vendasReader;
-    public static DocumentReader<Nf>? comprasReader;
-    public static Periodo? periodo;
+    public DocumentReader<Bom>? bomReader;
+    public DocumentReader<Nf>? vendasReader;
+    public DocumentReader<Nf>? comprasReader;
+    public Periodo periodo = new Periodo(6, 2024, 365);
 
-    public static CancellationToken cancellationToken;
+    [GlobalSetup]
+    public void Setup()
+    {
+        var basePath = Path.GetFullPath(Environment.GetEnvironmentVariable("INPUT_DIR") ?? Path.Combine(Environment.CurrentDirectory, "inputs"));
+        bomReader = new(Path.Combine(basePath, "boms"));
+        vendasReader = new(Path.Combine(basePath, "vendas"));
+        comprasReader = new(Path.Combine(basePath, "compras"));
+    }
 
     [Benchmark(Baseline = true)]
     public async Task RunSequencial()
@@ -26,9 +30,10 @@ public class BenchmarkContext
             .WithVendas(vendasReader!)
             .WithPeriodo(periodo!);
 
-        await svc.ExecuteAsync(cancellationToken);
+        await svc.ExecuteAsync(CancellationToken.None);
     }
 
+    [Benchmark]
     public async Task RunParallel()
     {
         var svc = new ConsumoDeSaldoParaleloService()
@@ -37,6 +42,6 @@ public class BenchmarkContext
             .WithVendas(vendasReader!)
             .WithPeriodo(periodo!);
 
-        await svc.ExecuteAsync(cancellationToken);
+        await svc.ExecuteAsync(CancellationToken.None);
     }
 }
